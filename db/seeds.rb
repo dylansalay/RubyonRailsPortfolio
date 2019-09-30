71ff34449713f4c639cdedf67cf64453c7b1a3d9
Dylan Salay <dylansalay@Dylans-MacBook-Pro.local>
2019-09-30 11:17:52 -0700

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ puts "5 skills created"
         title: "Portfolio title: #{portfolio_item}",
         subtitle: "My great service",
         body: "gnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-        main_image: "https://via.placeholder.com/600x400",
-        thumb_image: "https://via.placeholder.com/350x200" 
+        main_image: "https://placehold.it/600x400",
+        thumb_image: "https://placehold.it/350x200" 
     )
 end
 
